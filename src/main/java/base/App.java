123974a6456be3969e5ee4e35bd9a8aa6e9bec16
nic <mbnic@icloud.com>
2021-06-06 12:00:49 -0400

    final base commit

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,12 +3,29 @@ import java.util.Scanner;
 
 public class App {
     static Scanner in = new Scanner(System.in);
+    static double conversion = 0.09290304;
 
     public static void main(String[] args) {
         App myApp = new App();
         room myRoom = new room();
         myApp.getInput(myRoom);
 
+        int sqrFT = myApp.getSquareFeet(myRoom);
+        double sqrMeter = myApp.getSquareMeters(sqrFT);
+
+        myApp.printOutput(sqrFT, sqrMeter);
+    }
+
+    public void printOutput(int sqrFT, double sqrMeter) {
+        System.out.printf("The area is \n%d square feet\n%.3f square meters", sqrFT, sqrMeter);
+    }
+
+    public double getSquareMeters(int sqrFeet) {
+        return sqrFeet * conversion;
+    }
+
+    public int getSquareFeet(room room) {
+        return (room.length * room.width);
     }
 
     public void getInput(room room) {
